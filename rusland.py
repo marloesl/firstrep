aea017d744d2107c4a94954d654bc893e3723961
Marloes Leeuwis <Marloes@MacBook-Pro-van-Marloes.local>
2019-10-04 17:05:45 +0200

    first commit

---
+++ rusland.py
@@ -0,0 +1,12 @@
+rusland = input("Bestond Rusland 100 jaar voor Christus? ")
+oekraine = input("Bestond Oekraine 100 jaar voor Christus? ")
+
+def veluwe(vraag1, vraag2):
+    if vraag1 != "ja" and vraag2 != "ja":
+        return "Leugens!! De Volkskrant schrijft leugens!!"
+    else:
+        return "Er was een immigratiecrisis op de Veluwe"
+
+print(veluwe(rusland, oekraine))
+
+
